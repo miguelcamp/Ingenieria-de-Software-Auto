23b2176ba1de93e14d25b37086354dbc382c30bd
Fidel Alejandro Rojas Baptista <fidelrojas35@gmail.com>
2019-12-12 16:23:34 -0400

    Simular corregido

--- app.rb
+++ app.rb
@@ -31,43 +31,6 @@ post '/simulacion' do
     erb :simulacion
 end
 
-
-def avanzar()
-    if @orientacion == 'N'
-        @posY+=1
-    elsif @orientacion == 'S'
-        @posY-=1
-    elsif @orientacion == 'E'
-        @posX+=1
-    elsif @orientacion == 'O'
-        @posX-=1
-    end    
-end
-
-def izq()
-    if @orientacion == 'N'
-        @orientacion='O'
-    elsif @orientacion == 'S'
-        @orientacion='E'
-    elsif @orientacion == 'E'
-        @orientacion='N'
-    elsif @orientacion == 'O'
-        @orientacion='S'
-    end    
-end
-
-def der()
-    if @orientacion == 'N'
-        @orientacion='E'
-    elsif @orientacion == 'S'
-        @orientacion='O'
-    elsif @orientacion == 'E'
-        @orientacion='S'
-    elsif @orientacion == 'O'
-        @orientacion='N'
-    end    
-end
-
 get	'/'	do
     erb :index
 end
@@ -92,53 +55,8 @@ post '/simulacion' do
     erb :simulacion
 end
 
-
-def avanzar()
-    if @orientacion == 'N'
-        @posY+=1
-    elsif @orientacion == 'S'
-        @posY-=1
-    elsif @orientacion == 'E'
-        @posX+=1
-    elsif @orientacion == 'O'
-        @posX-=1
-    end    
-end
-
-def izq()
-    if @orientacion == 'N'
-        @orientacion='O'
-    elsif @orientacion == 'S'
-        @orientacion='E'
-    elsif @orientacion == 'E'
-        @orientacion='N'
-    elsif @orientacion == 'O'
-        @orientacion='S'
-    end    
-end
-
-def der()
-    if @orientacion == 'N'
-        @orientacion='E'
-    elsif @orientacion == 'S'
-        @orientacion='O'
-    elsif @orientacion == 'E'
-        @orientacion='S'
-    elsif @orientacion == 'O'
-        @orientacion='N'
-    end    
-end
-
 def simular()
-    #@auto.simular(@inst)
-    @inst.each_char {|i| 
-    case i
-    when 'A'
-        avanzar()
-    when 'I'
-        izq()
-    when 'D'
-        der()
-    end
-    }
+    autoo=Auto.new
+    autoo.simular(@inst)
+    @result=autoo.getEstado()
 end
